229df415399c6359d89d12e134199cd6cbcee9ca
Anirudh Kumar <51256968+Skylur-kross@users.noreply.github.com>
2022-12-13 20:36:01 +0530

    make changes

---
+++ checkVowelOrConsonant.js
@@ -0,0 +1,11 @@
+
+/**
+Step 1: crate an array of all the vowels
+step 2: take a string as input
+step 3: run a loop for the vowels array 
+step 4: compare every instance of the array to the string
+step 4.1: if matches then it is a vowels 
+step 5: if not then its a consonant. 
+**/
+
+const checkVowelOrConsonant = () => {};
