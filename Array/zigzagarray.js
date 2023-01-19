34dd1aadda5da419484a40653218944772083b62
Anirudh Kumar <51256968+Skylur-kross@users.noreply.github.com>
2023-01-19 20:55:11 +0530

    array functions

---
+++ Array/zigzagarray.js
@@ -0,0 +1,16 @@
+Array.prototype.zigZagArray = function () {
+
+}
+
+// immediate junior means
+// Vidhya Skill School8:30 PM
+// https://novoresume.com/?noRedirect=true
+// https://resume.io/
+// https://www.resumebuilder.com/
+// https://e-resume.vercel.app/builder
+// Vidhya Skill School8:36 PM
+// https://www.linkedin.com/feed/update/urn:li:activity:7017090534401843200/
+// Mahesh Patidar8:38 PM
+// wait
+// Vidhya Skill School8:40 PM
+// https://trello.com/en
