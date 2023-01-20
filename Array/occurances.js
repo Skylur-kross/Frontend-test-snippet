78080d746bcb852245102aaa85b27bf684a901ad
Anirudh Kumar <51256968+Skylur-kross@users.noreply.github.com>
2023-01-20 15:19:18 +0530

    array functions

--- Array/occurances.js
+++ Array/occurances.js
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
-<script>
     const input = "aaaabbaaccccccccccccccccccde";
 
     // Output
@@ -41,5 +26,3 @@
     console.log(result(input,"b"));
 
 
-
-</script>
